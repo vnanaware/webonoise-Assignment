c87158163f78d931df79db172db4239792246f8d
IBS <ibs@ibss-imac.local>
2019-03-20 17:50:42 +0530

    Initial Commit

---
+++ Teprature_Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Teprature_Test
+//
+//  Created by IBS on 3/20/19.
+//  Copyright © 2019 IBS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Teprature_TestTests/Teprature_TestTests.swift
@@ -0,0 +1,34 @@
+//
+//  Teprature_TestTests.swift
+//  Teprature_TestTests
+//
+//  Created by IBS on 3/20/19.
+//  Copyright © 2019 IBS. All rights reserved.
+//
+
+import XCTest
+@testable import Teprature_Test
+
+class Teprature_TestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
